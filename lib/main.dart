921980b1294121f70cf72fe90950606e2c662f34
Krishanu Bhattachajee <47850005+Kaiserb11@users.noreply.github.com>
2020-05-21 16:31:25 +0530

    added files

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:covid_app/pages/dashboard.dart';
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Covid Check',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Color(0XFF6D3FFF),
+        accentColor: Color(0XFF233C63),
+        fontFamily: 'Poppins',
+      ),
+      home: Dashboard(),
+    );
+  }
+}
+
+
